e3fae29aeeab4ec6c8bca964dd87523567323cf5
Paulo Peter <eduardopeter91@gmail.com>
2025-10-04 18:03:42 -0300

    Criando Tabelas

--- Biblioteca_Database.sql
+++ Biblioteca_Database.sql
@@ -54,5 +54,63 @@ CREATE DATABASE Biblioteca;
 
 USE Biblioteca;
 
+-- Obs: Criar inicialmente as tabelas que não possuem chave estrangeira (FK)
 
+CREATE TABLE Autor(
+    IdAutor SMALLINT IDENTITY, -- (Auto Incremento)
+    NomeAutor VARCHAR(50) NOT NULL,
+    SobrenomeAutor VARCHAR (60) NOT NULL,
+
+    CONSTRAINT pk_id_autor PRIMARY KEY (IdAutor)
+);
+
+sp_help Autor; -- Infos Gerais da Tabela Autor
+
+CREATE TABLE Editora(
+    IdEditora SMALLINT PRIMARY KEY IDENTITY,
+    NomeEditora VARCHAR(50) NOT NULL
+
+);
+
+sp_help Editora; -- Infos Gerais da Tabela Editora
+
+CREATE TABLE Assunto(
+    IdAssunto TINYINT PRIMARY KEY IDENTITY,
+    NomeAssunto VARCHAR(25) NOT NULL
+);
+
+sp_help Assunto; -- Infos Gerais da Tabela Assunto
+
+CREATE TABLE Livro(
+    IdLivro SMALLINT PRIMARY KEY IDENTITY(100,1), -- Iniciam em 100 adicionando de 1 em 1
+    NomeLivro VARCHAR(70) NOT NULL,
+    ISBN13 CHAR(13) NOT NULL UNIQUE, -- Sem repetições
+    DataPub DATE,
+    PrecoLivro MONEY NOT NULL,
+    NumeroPaginas SMALLINT NOT NULL,
+    IdEditora SMALLINT NOT NULL,
+    IdAssunto TINYINT NOT NULL,
+
+    CONSTRAINT fk_id_editora FOREIGN KEY(IdEditora) REFERENCES Editora(IdEditora) ON DELETE CASCADE,
+    CONSTRAINT fk_id_assunto FOREIGN KEY(IdAssunto) REFERENCES Assunto(IdAssunto) ON DELETE CASCADE,
+    CONSTRAINT verifica_preco CHECK(PrecoLivro >= 0)
+);
+
+sp_help Livro; -- Infos Gerais da Tabela Livro
+
+CREATE TABLE LivroAutor(
+    IdLivro SMALLINT NOT NULL,
+    IdAutor SMALLINT NOT NULL,
+
+    CONSTRAINT fk_id_livros FOREIGN KEY (IdLivro) REFERENCES Livro(IdLivro),
+    CONSTRAINT fk_id_autores FOREIGN KEY (IdAutor) REFERENCES Autor(IdAutor),
+
+    -- PK COMPOSTA
+    CONSTRAINT pk_livro_autor PRIMARY KEY(IdLivro, IdAutor)
+);
+
+sp_help Livro; -- Infos Gerais da Tabela LivroAutor
+
+-- LISTA AS TABELAS CRIADAS
+SELECT name FROM Biblioteca.sys.tables;
 
