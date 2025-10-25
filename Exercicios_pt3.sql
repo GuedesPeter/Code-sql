573343e4e60493a82daea7ddfc5a508b6059abd0
Paulo Peter <eduardopeter91@gmail.com>
2025-10-25 15:55:58 -0300

    🧩 Ex3_Pt2

--- Exercicios_pt3.sql
+++ Exercicios_pt3.sql
@@ -98,17 +98,98 @@ WHERE PrecoLivro NOT BETWEEN 30.00 AND 70.00;
 -- [À Fazer]
 
 -- 11. Mostre os livros cujo preço esteja entre 50 e 100 reais.
--- 12. Exiba os livros cuja data de publicação esteja entre 2018-01-01 e 2022-12-31.
+SELECT 
+    NomeLivro,
+    CONCAT('R$ ', PrecoLivro) AS Valor
+FROM Livro 
+WHERE PrecoLivro BETWEEN 50.00 AND 100.00;
+
+-- 12. Exiba os livros cuja data de publicação esteja entre 2010-01-01 e 2022-12-31.
+SELECT
+    NomeLivro,
+    DataPub AS Publicação
+FROM Livro
+WHERE DataPub BETWEEN '2010-01-01' AND '2022-12-31';
+
 -- 13. Liste os livros com número de páginas entre 200 e 500.
+SELECT
+    NomeLivro,
+    NumeroPaginas
+FROM Livro
+WHERE NumeroPaginas BETWEEN 200 AND 500;
 
 -- 14. Liste os livros cujo preço esteja entre 40 e 80 e o assunto não seja "Ficção".
--- 15. Exiba os livros cuja data de publicação esteja entre 2015 e 2020 ou o preço seja menor que 30.
--- 16. Liste os livros que tenham entre 300 e 600 páginas e editora diferente de "Companhia das Letras".
--- 17. Mostre os livros que tenham preço maior que 90 ou assunto "Economia", mas não da editora "Moderna".
--- 18. Exiba os livros cuja data de publicação esteja entre 2010 e 2018 e o preço não esteja entre 40 e 70.
--- 19. Liste os autores que não escreveram livros de assunto "Autoajuda" ou que publicaram livros com mais de 500 páginas.
--- 20. Mostre o nome dos livros que têm entre 200 e 400 páginas, preço menor que 100 e cuja editora seja "Pearson" ou "Atlas".
+SELECT 
+    L.NomeLivro AS Livro,
+    L.NumeroPaginas AS Páginas,
+    A.NomeAssunto AS Assunto
+FROM Livro L
+INNER JOIN Assunto A ON A.IdAssunto = L.IdAssunto
+WHERE PrecoLivro BETWEEN 40.00 AND 80.00
+AND A.NomeAssunto NOT LIKE '%Ficção%';
 
+-- 15. Exiba os livros cuja data de publicação esteja entre 2015 e 2020 ou o preço seja menor que 30.
+SELECT DISTINCT 
+*
+FROM Livro
+WHERE DataPub BETWEEN '2015' AND '2020'
+AND PrecoLivro > 30.00;
+
+-- 16. Liste os livros que tenham entre 300 e 600 páginas e editora diferente de "Companhia das Letras".
+SELECT DISTINCT
+    L.NomeLivro,
+    L.NumeroPaginas,
+    E.NomeEditora
+FROM Livro L
+JOIN Editora E ON E.IdEditora = L.IdEditora
+WHERE L.NumeroPaginas BETWEEN 300 AND 600
+AND E.NomeEditora != 'Companhia das Letras';
+
+
+-- 17. Mostre os livros que tenham preço maior que 90 ou assunto "Economia", mas não da editora "Moderna".
+SELECT DISTINCT
+    L.NomeLivro,
+    L.PrecoLivro,
+    A.NomeAssunto,
+    E.NomeEditora
+FROM Livro L 
+JOIN Editora E ON E.IdEditora = L.IdEditora
+JOIN Assunto A ON A.IdAssunto = L.IdAssunto
+WHERE L.PrecoLivro > 90 OR A.NomeAssunto = 'Economia'
+AND E.NomeEditora NOT LIKE '%Moderna%'; -- != "Moderna"
+
+-- 18. Exiba os livros cuja data de publicação esteja entre 2010 e 2018 e o preço não esteja entre 40 e 70.
+SELECT DISTINCT
+    NomeLivro,
+    PrecoLivro,
+    DataPub
+FROM Livro 
+WHERE DataPub BETWEEN '2010' AND '2018'
+AND PrecoLivro NOT BETWEEN 40.00 AND 70.00;
+
+-- 19. Liste os autores que não escreveram livros de assunto "Autoajuda" ou que publicaram livros com mais de 500 páginas.
+SELECT DISTINCT
+    CONCAT(A.NomeAutor, ' ',A.SobrenomeAutor) AS Autor,
+    L.NomeLivro,
+    L.NumeroPaginas,
+    AA.NomeAssunto
+FROM Autor A
+JOIN LivroAutor LA ON LA.IdAutor = A.IdAutor
+JOIN Livro L ON L.IdLivro = LA.IdLivro
+JOIN Assunto AA ON AA.IdAssunto = L.IdAssunto
+WHERE AA.NomeAssunto != 'Autoajuda' OR L.NumeroPaginas > 500;
+
+-- 20. Mostre o nome dos livros que têm entre 200 e 400 páginas, preço menor que 100 e cuja editora seja "Pearson" ou "Atlas".
+SELECT
+    L.NomeLivro,
+    L.NumeroPaginas,
+    L.PrecoLivro,
+    E.NomeEditora
+FROM Livro L 
+JOIN Editora E ON E.IdEditora = L.IdEditora
+WHERE L.NumeroPaginas BETWEEN 200 AND 400
+AND L.PrecoLivro < 100.00 
+AND E.NomeEditora = 'Pearson' OR E.NomeEditora = 'Atlas';
 
 
 
