3dc526e50c77114ea2549ce7ab716688db32795d
Paulo Peter <eduardopeter91@gmail.com>
2025-10-30 21:15:08 -0300

    🧩 EXERCÍCIOS pt1 – LIKE e NOT LIKE

--- Exercicios_pt6.sql
+++ Exercicios_pt6.sql
@@ -7,33 +7,111 @@ USE Biblioteca;
 
 /* 1. Liste todos os livros cujo nome comece com a letra 'A'. */
 
+SELECT
+   NomeLivro
+FROM Livro
+WHERE NomeLivro LIKE 'A%';
+
 /* 2. Mostre os livros cujo nome contenha a palavra 'História'. */
+
+SELECT
+   NomeLivro
+FROM Livro
+WHERE NomeLivro LIKE '%História%';
 
 /* 3. Encontre os livros cujo nome termine com a palavra 'Brasil'. */
 
-/* 4. Liste os livros cujo ISBN13 comece com '978-85'. */
+SELECT 
+   NomeLivro
+FROM Livro
+WHERE NomeLivro LIKE '%Brasil';
 
-/* 5. Exiba os livros cujo nome tenha exatamente 10 caracteres. */
+
+/* 4. Liste os livros cujo ISBN13 Termine com '978-05'. */
+
+SELECT
+   NomeLivro,
+   ISBN13
+FROM Livro
+WHERE ISBN13 LIKE '%05';
+
+/* 5. Exiba os livros cujo nome tenha exatamente 5 caracteres. */
+
+SELECT
+    NomeLivro
+FROM Livro
+WHERE LEN(NomeLivro) = 5;
+
 
 /* 6. Mostre os autores cujo sobrenome comece com a letra 'S'. */
 
+SELECT
+   CONCAT(NomeAutor,' ',SobrenomeAutor)
+FROM Autor
+WHERE SobrenomeAutor LIKE 'S%';
+
 /* 7. Liste os autores cujo nome contenha a sequência 'mar'. */
+
+SELECT
+   CONCAT(NomeAutor,' ',SobrenomeAutor) AS AUTOR
+FROM Autor
+WHERE NomeAutor LIKE '%MAR%';
 
 /* 8. Selecione os autores cujo sobrenome termine com 'son'. */
 
+SELECT
+   CONCAT(NomeAutor,' ',SobrenomeAutor) AS AUTOR
+FROM Autor
+WHERE NomeAutor LIKE '%SON';
+
 /* 9. Mostre as editoras cujo nome contenha a palavra 'Editora'. */
+
+SELECT
+   NomeEditora
+FROM Editora
+WHERE NomeEditora LIKE '%Editora%';
 
 /* 10. Liste as editoras cujo nome comece com as letras entre 'A' e 'C'. */
 
+SELECT
+   NomeEditora
+FROM Editora
+WHERE NomeEditora LIKE 'B%';
+
 /* 11. Exiba os assuntos cujo nome termine com 'logia'. */
+
+SELECT
+   NomeAssunto
+FROM Assunto
+WHERE NomeAssunto LIKE '%LOGIA';
 
 /* 12. Liste os assuntos cujo nome comece com a letra 'F'. */
 
+SELECT
+   NomeAssunto
+FROM Assunto
+WHERE NomeAssunto LIKE 'F%';
+
 /* 13. Mostre os livros cujo nome contenha um hífen '-'. */
+
+SELECT
+   NomeLivro
+FROM Livro
+WHERE NomeLivro LIKE '%-%';
 
 /* 14. Encontre os livros cujo nome não comece com a letra 'O'. */
 
+SELECT
+   NomeLivro
+FROM Livro
+WHERE NomeLivro NOT LIKE 'O%';
+
 /* 15. Liste os autores cujo nome tenha a letra 'a' na segunda posição. */
+
+SELECT
+   CONCAT(NomeAutor,' ',SobrenomeAutor) AS AUTOR
+FROM Autor
+WHERE NomeAutor LIKE '_A%';
 
 /* 16. Mostre os livros cujo nome contenha dois espaços (ex: títulos compostos). */
 
